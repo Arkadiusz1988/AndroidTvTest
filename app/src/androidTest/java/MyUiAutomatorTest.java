277b073014de42f785235fd207216957f843d976
Arkadiusz Hojenski <arkadiusz.hojenski@gmail.com>
2019-04-18 16:41:26 +0200

    check Wifi is connected/not connected.

--- app/src/androidTest/java/MyUiAutomatorTest.java
+++ app/src/androidTest/java/MyUiAutomatorTest.java
@@ -6,13 +6,13 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.*;
 import com.example.arkadiusz.androidtvtest.MainActivity;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 
 import java.io.File;
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
 
 @RunWith(AndroidJUnit4.class)
 public class MyUiAutomatorTest {
@@ -58,8 +58,88 @@ public class MyUiAutomatorTest {
         appsTab.click();
     }
 
+  @Test
+  public void checkWifiConnected() throws UiObjectNotFoundException {
+
+    mDevice.pressHome();
+
+    UiObject start = mDevice.findObject(new UiSelector().textStartsWith("Wi-Fi is not"));
+
+    if (!(start.waitForExists(9000))) {
+      UiObject allAppsButton = mDevice.findObject(new UiSelector().description("Apps"));
+      allAppsButton.waitForExists(10);
+      mDevice.pressDPadUp();
+      mDevice.pressDPadRight();
+      mDevice.pressDPadRight();
+      mDevice.pressDPadRight();
+      mDevice.pressDPadCenter();
+      mDevice.pressDPadCenter();
+      UiObject networkAndInternet =
+          mDevice.findObject(new UiSelector().textContains("Network & Internet"));
+      networkAndInternet.waitForExists(5000);
+      //        mDevice.pressDPadDown();
+      mDevice.pressDPadCenter();
+      UiObject notification = mDevice.findObject(new UiSelector().textStartsWith("Wi-fi"));
+      notification.waitForExists(5000);
+      Assert.assertEquals(true, notification.waitForExists(5000));
+
+      //        if(mDevice.findObject(new UiSelector().text("Videos by Google")).exists()){
+      //            mDevice.pressDPadRight();
+      //            mDevice.pressDPadRight();
+      //        }
+      //        UiObject appsTab = mDevice.findObject(new UiSelector().text("AndroidTvTest"));
+      //        appsTab.click();
+        } else {
+                Assert.assertFalse("wifi is not connected",true);
+        }
+
+    }
+
     @Test
-    public void checkAppsGoogleContent() throws UiObjectNotFoundException {
+    public void checkWifiNotConnected() throws UiObjectNotFoundException {
+
+        mDevice.pressHome();
+        UiObject start = mDevice.findObject(new UiSelector().textStartsWith("Wi-Fi is not"));
+
+    if (start.waitForExists(9000)) {
+
+      UiObject allAppsButton = mDevice.findObject(new UiSelector().description("Apps"));
+      allAppsButton.waitForExists(10);
+      mDevice.pressDPadUp();
+      mDevice.pressDPadUp();
+      mDevice.pressDPadRight();
+      mDevice.pressDPadCenter();
+      UiObject settings = mDevice.findObject(new UiSelector().textContains("Settings"));
+      settings.waitForExists(5000);
+      mDevice.pressDPadCenter();
+      UiObject networkAndInternet =
+          mDevice.findObject(new UiSelector().textContains("Network & Internet"));
+      networkAndInternet.waitForExists(5000);
+      mDevice.pressDPadCenter();
+      UiObject notification = mDevice.findObject(new UiSelector().textStartsWith("AndroidWifi"));
+      notification.waitForExists(5000);
+      Assert.assertEquals(true, notification.waitForExists(5000));
+      //        mDevice.pressDPadCenter();
+      //        mDevice.pressDPadDown();
+      //        mDevice.pressDPadDown();
+
+      //        mDevice.pressDPadRight();
+      //        mDevice.pressDPadRight();
+      //        mDevice.pressDPadCenter();
+      //        mDevice.pressDPadCenter();
+      //        UiObject networkAndInternet = mDevice.findObject(new
+      // UiSelector().textContains("Network & Internet"));
+      //        networkAndInternet.waitForExists(5000);
+      //        mDevice.pressDPadCenter();
+      //        UiObject notification = mDevice.findObject(new
+      // UiSelector().textStartsWith("Wi-fi"));
+      //        notification.waitForExists(5000);
+      //        Assert.assertEquals(true,notification.waitForExists(5000));
+     }
+    }
+
+    @Test
+    public void checkAppsGoogleContent() throws UiObjectNotFoundException, InterruptedException, IOException {
 
         mDevice.pressHome();
         UiObject allAppsButton = mDevice.findObject(new UiSelector().description("Apps"));
@@ -73,12 +153,23 @@ public class MyUiAutomatorTest {
         appsTab.click();
         UiObject allAppsButton1 = mDevice.findObject(new UiSelector().textContains("Zeitgeist"));
         allAppsButton1.clickAndWaitForNewWindow();
+        System.out.println(mDevice.takeScreenshot(new File("screenshot-file-name")));
+        mDevice.takeScreenshot(new File("/sdcard/Download/test.png"));
         mDevice.pressDPadRight();
         mDevice.pressDPadCenter();
         UiObject allAppsButton2 = mDevice.findObject(new UiSelector().textContains("Google Zeitgeist"));
         allAppsButton2.clickAndWaitForNewWindow();
         mDevice.pressDPadRight();
         mDevice.pressDPadCenter();
+        sleep(3000);
+        File path = new File("screenshot-file-name.png");
+//        mDevice.executeShellCommand("adb shell screencap -p /media/arkadiusz/7A8C460A8C45C0FD/screencap.png");
+//        mDevice.executeShellCommand("adb pull /media/arkadiusz/7A8C460A8C45C0FD/screencap.png");
+        sleep(3000);
+        int SDK_VERSION = android.os.Build.VERSION.SDK_INT;
+        if (SDK_VERSION >= 17) {
+            mDevice.takeScreenshot(path);
+        }
     }
 
     private String getLauncherPackageName() {
@@ -92,6 +183,6 @@ public class MyUiAutomatorTest {
 
     @After
     public void tearDown() {
-        mDevice.takeScreenshot(new File("/media/arkadiusz/7A8C460A8C45C0FD/projekty_AndroidTv/screenshot-file-name.png"));
+        mDevice.takeScreenshot(new File("screenshot-file-name.png"));
     }
 }
